72c31c18c155b1100066b2a087d456e8f7311d4a
Dmitry <sdvkam@gmail.com>
2022-01-06 12:26:25 +1200

    Application: recipes and his models_2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -12,12 +12,26 @@ class Tag(models.Model):
     slug = models.SlugField(
         'Английское имя для тега', unique=True)
 
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(
         'Название ингридиента', max_length=200, unique=True)
     measurement_unit = models.CharField(
         'Единица измерения', max_length=10)
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Amount_Ingredient(models.Model):
@@ -27,6 +41,13 @@ class Amount_Ingredient(models.Model):
     amount = models.PositiveIntegerField(
         'Количество ингридиента')
 
+    class Meta:
+        verbose_name = 'Количество ингридента'
+        verbose_name_plural = 'Список ингридентов и их количество'
+
+    def __str__(self):
+        return self.ingredient
+
 
 class Recipe(models.Model):
     author = models.ForeignKey(
@@ -34,6 +55,11 @@ class Recipe(models.Model):
         related_name='recipes')
     name = models.CharField(
         'Название рецепта', max_length=200)
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        help_text='Введите дату',
+        auto_now_add=True
+    )
     image = models.ImageField(
         'Фотография блюда', upload_to='recipes/')
     text = models.TextField(
@@ -45,3 +71,11 @@ class Recipe(models.Model):
         'Tag', verbose_name='Тег', related_name='recipes')
     cooking_time = models.PositiveIntegerField(
         'Время приготовления')
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
