424a96f368fbb6428e302f35289883d57d1fa7fe
Dmitry <sdvkam@gmail.com>
2022-02-09 19:08:39 +1200

    settings + readme

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,9 +30,9 @@ DICT_TRANSLIT_RUS_TO_ENGLISH = make_dict_tranlit()
 SECRET_KEY = 'oilc+h(xg4rsq8l72*a4!ki5m1)!g_4%pz!ar5q%7q27w*_ys9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', os.getenv('HOST')]
 
 # Application definition
 
